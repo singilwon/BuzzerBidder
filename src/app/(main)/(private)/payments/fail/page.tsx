8333b9fb63070141e4f3a1389dd9c71a0b5be7e5
Song Joowon <joowoni98@gmail.com>
2026-05-17 15:56:03 +0900

    ♻️ 결제 결과 화면 공통화

--- src/app/(main)/(private)/payments/fail/page.tsx
+++ src/app/(main)/(private)/payments/fail/page.tsx
@@ -1,7 +1,12 @@
+import PaymentResult from "@/components/payments/PaymentResult";
+
 export default function Page() {
   return (
-    <>
-      <h1>FailPage</h1>
-    </>
+    <PaymentResult
+      title="결제에 실패했습니다"
+      description="결제 정보를 확인한 뒤 다시 시도해주세요."
+      buttonText="마이페이지로 이동"
+      href="/mypage/bizz"
+    />
   );
 }
